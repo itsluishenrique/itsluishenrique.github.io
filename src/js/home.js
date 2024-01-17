d2a943c6e390be77c0abf2884f4600cf304096c4
itsluiswork <iamluishenrique.work@gmail.com>
2024-01-17 06:14:35 -0300

    v10

--- src/js/home.js
+++ src/js/home.js
@@ -1,3 +1,3 @@
 $(document).ready(function () {
-    window.setTimeout(Load, 10000)
+    window.setTimeout(Load, 5000)
 });
--- src/js/load.js
+++ src/js/load.js
@@ -1,9 +1,7 @@
-function Load() {
-    //$('.loading-wrapper').css('display', 'block');
+function Load() {    
+    $('#navigation').css('display', 'block');
     
-    //$('#navigation').css('display', 'block');
+    $('#main').css('display', 'block');
     
-    //$('#main').css('display', 'block');
-    
-    //$('.loading-wrapper').css('display', 'none');
+    $('#loading-wrapper').css('display', 'none');
 }
