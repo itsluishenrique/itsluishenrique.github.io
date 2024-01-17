186cdd6973605dd9170e672d971c07e23a6b9cb9
itsluiswork <iamluishenrique.work@gmail.com>
2024-01-17 06:08:37 -0300

    v7

--- src/js/load.js
+++ src/js/load.js
@@ -1,3 +1,9 @@
 function Load() {
+    $('.loading-wrapper').css('display', 'block');
     
+    $('#navigation').css('display', 'block');
+    
+    $('#main').css('display', 'block');
+    
+    $('.loading-wrapper').css('display', 'none');
 }
