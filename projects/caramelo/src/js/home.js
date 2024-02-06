c72d8a1415a5ff2a0ba19bdf682d8885af30c86b
itsluiswork <iamluishenrique.work@gmail.com>
2024-02-05 23:19:58 -0300

    v 050224

---
+++ projects/caramelo/src/js/home.js
@@ -0,0 +1,3 @@
+$(document).ready(function () {
+    window.setTimeout(Load, 2000)
+});
---
+++ projects/caramelo/src/js/load.js
@@ -0,0 +1,9 @@
+function Load() {
+    $('#loading-wrapper').css('display', 'none');
+
+    $('#header').css('display', 'block');
+
+    $('#principal').css('display', 'block');
+
+    $('#footer').css('display', 'block');
+}
